2b5c25a28dc9d279759100fbc2486c84565fde0d
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2022-01-15 19:36:27 +0000

    created stack and queue

---
+++ libs/stack.ts
@@ -0,0 +1,38 @@
+class Stack <T> {
+    private array: T[];
+
+    constructor(initialArray: T[] = new Array()) {
+        this.array = initialArray;
+    }
+
+    public get top(): T | null {
+        const value = this.array.at(-1);
+
+        if(value == undefined) {
+            console.warn("Unable to access element from empty stack");
+            return null;
+        }
+        else {
+            return value;
+        }
+    }
+
+    public push(element: T): number {
+        return this.array.push(element);
+    }
+
+    public pop(): T | null {
+        const value = this.array.pop();
+
+        if(value == undefined) {
+            console.warn("Unable to pop element from empty stack");
+            return null;
+        }
+        else {
+            return value;
+        }
+    }
+}
+
+
+export default Stack;
